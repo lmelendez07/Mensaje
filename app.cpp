326fbd986c553c5a7bc8ee082b68785ee6b3013d
lmelendez <luisr.melendez07@gmail.com>
2023-02-19 20:25:06 -0600

    Subir aplicacion

---
+++ app.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string nombre;
+    cout << "Por favor, ingresa tu nombre: ";
+    cin >> nombre;
+    cout << "¡Hola, " << nombre << "! Bienvenido." << endl;
+    return 0;
+}
